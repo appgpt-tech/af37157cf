27f86ca3dd50d6693e42dd42c3e44112f13e9495
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 16:14:36 +0200

    edited on Wed Jan 31 2024 16:14:26 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{}};
+let data: any = {"defaultData":{"Books":[{"title":"Book 1","author":"Author 1","genre":"Genre 1","bookCover":"URL 1"},{"title":"Book 2","author":"Author 2","genre":"Genre 2","bookCover":"URL 2"},{"title":"Book 3","author":"Author 3","genre":"Genre 3","bookCover":"URL 3"}],"Authors":[{"name":"Author 1","books":"Book 1, Book 2"},{"name":"Author 2","books":"Book 3, Book 4"},{"name":"Author 3","books":"Book 5, Book 6"}],"Genres":[{"category":"Genre 1"},{"category":"Genre 2"},{"category":"Genre 3"}]}};
 export default data;
