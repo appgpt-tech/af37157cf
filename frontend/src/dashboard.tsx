f2c425bd82ff55ff5b98972b676730e739b37bcf
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 16:12:13 +0200

    edited on Wed Jan 31 2024 16:12:04 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "Libby" />
-      <CardContent> book archive < /CardContent>
+      <CardHeader title= "" />
+      <CardContent> Built by appGPT < /CardContent>
       </Card>
     );
