5369b7686609fa2516bacb7909da77a8a5f5ebc7
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 16:12:14 +0200

    edited on Wed Jan 31 2024 16:12:04 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[{"title":"In Search of Lost Time","author":"Marcel Proust"},{"title":"Don Quixote","author":"Miguel de Cervantes"},{"title":"Ulysses","author":"James Joyce"}]}};
+let data: any = {"defaultData":{}};
 export default data;
