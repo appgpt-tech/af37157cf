84b3f177107e75dde2b068bf45c3acccf5ca660a
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 23:48:32 +0200

    edited on Fri Feb 02 2024 23:48:11 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,9 +3,7 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { booksEntity } from "./db/books.entity";
-import { authorsEntity } from "./db/authors.entity";
-import { genresEntity } from "./db/genres.entity";
+
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -16,7 +14,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, booksEntity, authorsEntity, genresEntity];
+    dbConfig.entities = [SettingsEntity, ];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -26,16 +24,14 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"books":[{"title":"Book Title 1","author":"Author 1","genre":"Genre 1","bookCover":"URL 1"},{"title":"Book Title 2","author":"Author 2","genre":"Genre 2","bookCover":"URL 2"},{"title":"Book Title 3","author":"Author 3","genre":"Genre 3","bookCover":"URL 3"}],"authors":[{"name":"Author 1","books":["Book Title 1","Book Title 3"]},{"name":"Author 2","books":["Book Title 2"]},{"name":"Author 3","books":["Book Title 1"]}],"genres":[{"category":"Genre 1"},{"category":"Genre 2"},{"category":"Genre 3"}]};
+    let data: any = {};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("booksEntity", data.books);
-await this.SeedResource("authorsEntity", data.authors);
-await this.SeedResource("genresEntity", data.genres); 
+       
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
