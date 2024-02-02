faba3bafec17010661d581ef3268b19de54892e9
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 23:48:21 +0200

    edited on Fri Feb 02 2024 23:48:10 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -2,16 +2,19 @@
     import { mergeTranslations } from "ra-core";
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
+import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"books":{"name":"Books","fields":{"title":"title","author":"author","genre":"genre","bookCover":"bookCover","id":"id"}},"authors":{"name":"Authors","fields":{"name":"name","books":"books","id":"id"}},"genres":{"name":"Genres","fields":{"category":"category","id":"id"}}}};
+    const enResources = { resources: {}};
+const frResources = { resources: {}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
+const fr = mergeTranslations(frOriginal,frResources);
 
-    const translations = { en};
+    const translations = { en, fr};
     export const i18nProvider = polyglotI18nProvider(
       (locale) => translations[locale],
       "en", //default locale
-      [{"locale":"en","name":"English"}]
+      [{"locale":"en","name":"English"},{"locale":"fr","name":"Français"}]
     );
     
