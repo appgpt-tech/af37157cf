c8492b5fbdd3b1622a94d366fcb10fc9b491da35
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 22:19:32 +0200

    edited on Mon Feb 05 2024 22:19:22 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "PokeVault" />
-      <CardContent> A web app to keep a Pokémon collectible cards inventory < /CardContent>
+      <CardHeader title= "" />
+      <CardContent> Built by appGPT < /CardContent>
       </Card>
     );
