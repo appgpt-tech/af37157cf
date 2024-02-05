6524b4d3c84b9d26f173d90bac95dc6c906a6463
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 20:09:45 +0200

    edited on Mon Feb 05 2024 20:09:34 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{}};
+let data: any = {"defaultData":{"Users":[{"userID":"U001","email":"email@example.com","name":"John Doe"}],"Cards":[{"cardID":"C001","setID":"S001","cardName":"Card 1","serial":"1234","type":"Fire","rarity":"Rare","condition":"New","imageURL":"http://example.com/image.jpg"}],"Sets":[{"setID":"S001","setName":"Set 1","releaseDate":"2022-01-01","totalCards":100}],"Inventory":[{"userID":"U001","cardID":"C001","recordedDate":"2022-01-01"}],"Wishlist":[{"userID":"U001","cardID":"C001","insertedDate":"2022-01-01"}]}};
 export default data;
