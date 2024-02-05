2bac305b0b90fda25f6d6909c32023e06cfd5541
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 20:09:54 +0200

    created on Mon Feb 05 2024 20:09:34 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/Wishlist.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const WishlistTitle = () => {
+  const record = useRecordContext();
+  return <span>Wishlist {record ? `"${ record.userID }"` : ""}</span>;
+};
+
+export const WishlistList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="userID" />
+<TextField source="cardID" />
+<TextField source="insertedDate" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const WishlistEdit = () => (
+                    <Edit title={<WishlistTitle />}>
+                      <SimpleForm>
+                          <TextInput source="userID"   />
+<TextInput source="cardID"   />
+<TextInput source="insertedDate"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const WishlistCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="userID"   />
+<TextInput source="cardID"   />
+<TextInput source="insertedDate"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+
+    ];
+
+
