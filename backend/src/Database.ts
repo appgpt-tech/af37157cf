2c3bea30899119fc74e7b87504e54af87a85b4c5
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 20:10:05 +0200

    edited on Mon Feb 05 2024 20:09:34 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,7 +3,11 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-
+import { UsersEntity } from "./db/Users.entity";
+import { CardsEntity } from "./db/Cards.entity";
+import { SetsEntity } from "./db/Sets.entity";
+import { InventoryEntity } from "./db/Inventory.entity";
+import { WishlistEntity } from "./db/Wishlist.entity";
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -14,7 +18,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, ];
+    dbConfig.entities = [SettingsEntity, UsersEntity, CardsEntity, SetsEntity, InventoryEntity, WishlistEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -24,14 +28,18 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {};
+    let data: any = {"Users":[{"userID":"U001","email":"email@example.com","name":"John Doe"}],"Cards":[{"cardID":"C001","setID":"S001","cardName":"Card 1","serial":"1234","type":"Fire","rarity":"Rare","condition":"New","imageURL":"http://example.com/image.jpg"}],"Sets":[{"setID":"S001","setName":"Set 1","releaseDate":"2022-01-01","totalCards":100}],"Inventory":[{"userID":"U001","cardID":"C001","recordedDate":"2022-01-01"}],"Wishlist":[{"userID":"U001","cardID":"C001","insertedDate":"2022-01-01"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-       
+      await this.SeedResource("UsersEntity", data.Users);
+await this.SeedResource("CardsEntity", data.Cards);
+await this.SeedResource("SetsEntity", data.Sets);
+await this.SeedResource("InventoryEntity", data.Inventory);
+await this.SeedResource("WishlistEntity", data.Wishlist); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
