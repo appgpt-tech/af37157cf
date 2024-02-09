b63a7bd8060f917b273f5bd408206b78a941dc54
11ai <124818466+11ai@users.noreply.github.com>
2024-02-09 21:51:57 +0200

    created on Fri Feb 09 2024 21:51:37 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/SupportTickets.tsx
@@ -0,0 +1,112 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const SupportTicketsTitle = () => {
+  const record = useRecordContext();
+  return <span>SupportTickets {record ? `"${ record.ticketId }"` : ""}</span>;
+};
+
+export const SupportTicketsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="ticketId" />
+<ReferenceField source="userId" reference="Users"  />
+<ReferenceField source="customerId" reference="Customers"  />
+
+<TextField source="status" />
+<DateField source="creationDate" />
+<DateField source="resolutionDate" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const SupportTicketsEdit = () => (
+                    <Edit title={<SupportTicketsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="ticketId"   />
+<ReferenceInput source="userId"  reference="Users"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<TextInput source="description"   />
+<TextInput source="status"   />
+<DateInput source="creationDate"   />
+<DateInput source="resolutionDate"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const SupportTicketsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="ticketId"   />
+<ReferenceInput source="userId"  reference="Users"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<TextInput source="description"   />
+<TextInput source="status"   />
+<DateInput source="creationDate"   />
+<DateInput source="resolutionDate"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="userId" label="userId" reference="Users"   alwaysOn/>,
+<ReferenceInput source="customerId" label="customerId" reference="Customers"   alwaysOn/>,
+,
+,
+,
+,
+
+    ];
+
+
