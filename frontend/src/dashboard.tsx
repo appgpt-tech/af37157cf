282281365e56c97ba879702b040eb6c009c7c44b
11ai <124818466+11ai@users.noreply.github.com>
2024-02-09 21:51:48 +0200

    edited on Fri Feb 09 2024 21:51:37 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "" />
-      <CardContent> Built by appGPT < /CardContent>
+      <CardHeader title= "FitPulse" />
+      <CardContent> My app is a Health and Fitness app.This app caters to the growing interest in health and wellness people nowdays have. < /CardContent>
       </Card>
     );
