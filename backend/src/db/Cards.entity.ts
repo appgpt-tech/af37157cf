9923d10bc04ead654a59df2214dd1109ce7feddf
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 03:17:52 +0200

    edited on Sat Feb 10 2024 03:17:20 GMT+0200 (Eastern European Standard Time)

--- backend/src/db/Cards.entity.ts
+++ backend/src/db/Cards.entity.ts
@@ -9,8 +9,8 @@ export class CardsEntity {
   @Column("text",{nullable: true})
 cardId: string;
 
-@Column("text",{nullable: true})
-setid: string;
+@Column("integer",{nullable: true})
+setid: number;
 
 @Column("text",{nullable: true})
 cardName: string;
