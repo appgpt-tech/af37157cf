6faae8345def296c8f39d833db5c7eb96d8f337f
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 03:17:55 +0200

    edited on Sat Feb 10 2024 03:17:20 GMT+0200 (Eastern European Standard Time)

--- backend/src/db/Inventory.entity.ts
+++ backend/src/db/Inventory.entity.ts
@@ -6,11 +6,11 @@ export class InventoryEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column("text",{nullable: true})
-userId: string;
+  @Column("integer",{nullable: true})
+userId: number;
 
-@Column("text",{nullable: true})
-cardId: string;
+@Column("integer",{nullable: true})
+cardId: number;
 
 @Column("date",{nullable: true})
 recordedDate: Date;
