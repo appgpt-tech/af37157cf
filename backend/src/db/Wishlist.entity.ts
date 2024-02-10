c770e787075986e98e293adaffc29e317f9d8610
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 03:05:51 +0200

    edited on Sat Feb 10 2024 03:05:15 GMT+0200 (Eastern European Standard Time)

--- backend/src/db/Wishlist.entity.ts
+++ backend/src/db/Wishlist.entity.ts
@@ -6,11 +6,11 @@ export class WishlistEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column("integer",{nullable: true})
-userId: number;
+  @Column("text",{nullable: true})
+userId: string;
 
-@Column("integer",{nullable: true})
-cardId: number;
+@Column("text",{nullable: true})
+cardId: string;
 
 @Column("date",{nullable: true})
 insertedDate: Date;
