17ef5f59c5b690b0b64ac31eb16f692e98ff51bd
11ai <124818466+11ai@users.noreply.github.com>
2024-02-12 20:21:21 +0200

    edited on Mon Feb 12 2024 20:21:12 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -2,6 +2,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
       <CardHeader title= "PokeVault" />
-      <CardContent> My app is a web app to keep a Pokémon collectible cards inventory which involves blending themes of collection, organization, and the vibrant world of Pokémon. < /CardContent>
+      <CardContent> A web app to keep a Pokémon collectible cards inventory which involves blending themes of collection, organization, and the vibrant world of Pokémon. < /CardContent>
       </Card>
     );
