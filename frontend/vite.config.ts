bfad6f1f3dff43688ef64ff4a375ea530b9e26a7
11ai <124818466+11ai@users.noreply.github.com>
2024-02-24 20:58:25 +0200

    edited on Sat Feb 24 2024 20:58:24 GMT+0200 (Eastern European Standard Time)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,16 @@
-
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  // https://vitejs.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-    base: '/af37157cf/',
-    build: {
-      outDir: '.output/af37157cf',
-      emptyOutDir: true
-    },
-    server: {
-      host: true,
-      strictPort: true,
-      port: 5173
-    }
-  })
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/af37157cf/",
+  build: {
+    outDir: ".output/af37157cf",
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
+})
