2f9384d9dd5cc817ca39a9c6fdc09088c043313a
11ai <124818466+11ai@users.noreply.github.com>
2024-02-29 18:00:44 +0200

    edited on Thu Feb 29 2024 18:00:26 GMT+0200 (Eastern European Standard Time)

--- frontend/src/resources/Sets.tsx
+++ frontend/src/resources/Sets.tsx
@@ -54,15 +54,15 @@ const ListActions = () => (
 );
 const SetsTitle = () => {
   const record = useRecordContext();
-  return <span>Sets {record ? `"${ record.setName }"` : ""}</span>;
+  return <span>Sets {record ? `"${ record.id }"` : ""}</span>;
 };
 
 export const SetsList = () => (
       <List actions={<ListActions  />} filters={ResourceFilters} >
         <DatagridConfigurable>
-          <TextField source="setName" />
-<DateField source="releaseDate" />
-<NumberField source="totalCards" /><EditButton />
+          <TextField source="SetName" />
+<DateField source="ReleaseDate" />
+<NumberField source="TotalCards" /><EditButton />
 
         </DatagridConfigurable>
       </List>
@@ -73,11 +73,11 @@ export const SetsEdit = () => (
                       <SimpleForm>
                         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
                           <Grid item xs={4}>
-<TextInput source="setName"   /></Grid>
+<TextInput source="SetName"   /></Grid>
 <Grid item xs={4}>
-<DateInput source="releaseDate"   /></Grid>
+<DateInput source="ReleaseDate"   /></Grid>
 <Grid item xs={4}>
-<NumberInput source="totalCards"   /></Grid>
+<NumberInput source="TotalCards"   /></Grid>
                         </Grid>
                       </SimpleForm>
                     </Edit>
@@ -88,11 +88,11 @@ export const SetsCreate = () => (
                                     <SimpleForm>
                                       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
                                         <Grid item xs={4}>
-<TextInput source="setName"   /></Grid>
+<TextInput source="SetName"   /></Grid>
 <Grid item xs={4}>
-<DateInput source="releaseDate"   /></Grid>
+<DateInput source="ReleaseDate"   /></Grid>
 <Grid item xs={4}>
-<NumberInput source="totalCards"   /></Grid>
+<NumberInput source="TotalCards"   /></Grid>
                                       </Grid>
                                     </SimpleForm>
                                   </Create>
