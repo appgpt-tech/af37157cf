b3b1d03ddf25d5f73e60620a63ec080e89ddd3db
11ai <124818466+11ai@users.noreply.github.com>
2024-02-29 18:32:12 +0200

    edited on Thu Feb 29 2024 18:32:00 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -3,9 +3,9 @@
  import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title="PokeVault" />
+      <CardHeader title="FitPulse" />
       <CardContent> 
-      A web app to keep a Pokémon collectible cards inventory which involves blending themes of collection, organization, and the vibrant world of Pokémon.
+      Health and Fitness app.This app caters to the growing interest in health and wellness people nowdays have.
       <p>Built with{' '}
       <a
         href="https://www.appgpt.tech"
