2e8b82e7b8077fd20ef659714f77bae5704a9f7f
11ai <124818466+11ai@users.noreply.github.com>
2024-02-29 18:00:34 +0200

    edited on Thu Feb 29 2024 18:00:25 GMT+0200 (Eastern European Standard Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -85,19 +85,19 @@ const App = () => (
 list={UsersList}
 create={UsersCreate}
 edit={UsersEdit}
-recordRepresentation="userName"
+recordRepresentation="id"
 icon={UsersIcon}/>
 <Resource name="Cards" options={{label:"Cards"}} 
 list={CardsList}
 create={CardsCreate}
 edit={CardsEdit}
-recordRepresentation="cardName"
+recordRepresentation="id"
 icon={CardsIcon}/>
 <Resource name="Sets" options={{label:"Sets"}} 
 list={SetsList}
 create={SetsCreate}
 edit={SetsEdit}
-recordRepresentation="setName"
+recordRepresentation="id"
 icon={SetsIcon}/>
 <Resource name="Inventory" options={{label:"Inventory"}} 
 list={InventoryList}
